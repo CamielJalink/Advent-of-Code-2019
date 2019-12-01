bd10afc09dd98158d495bc32ae9269933dbc3a0b
Camiel <camiel.jalink@gmail.com>
2019-12-01 12:48:38 +0100

    Day1 part1 solution code

--- day1/index.ts
+++ day1/index.ts
@@ -4,12 +4,29 @@ import { promisify } from "util";
 // promisify the readFile node method to read our txt input files.
 let readInput = promisify(readFile);
 
-function advent(){
-  return readInput("input.txt", "utf8").then((input: string) => {
-    console.log(input);
-  })
+function fuelForModule(mass: number){
+  let fuel: number = Math.floor((mass / 3)) - 2;
+  return fuel;
+}
 
-  
+
+function advent(){
+
+  return readInput("input.txt", "utf8").then((input: string) => {
+    let inputStringArray: string[] = input.split("\n");
+    let inputArray: number[] = [];
+
+    inputStringArray.forEach((module) => {
+      inputArray.push(parseInt(module))
+    })
+
+    let totalFuelNeeded: number = 0;
+    inputArray.forEach((module) => {
+      totalFuelNeeded += fuelForModule(module);
+    })
+
+    console.log(totalFuelNeeded);
+  })
 }
 
 advent();
