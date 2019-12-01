8c9fca477c2b821c26341e695cea8ed308ed684c
Camiel <camiel.jalink@gmail.com>
2019-12-01 11:50:09 +0100

    boilerplate update to include promisify and readfile from node

---
+++ dayX/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = require("fs");
+var util_1 = require("util");
+// promisify the readFile node method to read our txt input files.
+var readInput = util_1.promisify(fs_1.readFile);
+function advent() {
+}
+advent();
---
+++ dayX/index.ts
@@ -0,0 +1,11 @@
+import { readFile } from "fs";
+import { promisify } from "util";
+
+// promisify the readFile node method to read our txt input files.
+let readInput = promisify(readFile);
+
+function advent(){
+
+}
+
+advent();
