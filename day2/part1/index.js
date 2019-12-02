f592022ca99d97cd8440807d43f70c579d19659a
Camiel <camiel.jalink@gmail.com>
2019-12-02 07:49:27 +0100

    Some parsing of the day2part1 input

--- day2/part1/index.js
+++ day2/part1/index.js
@@ -4,9 +4,20 @@ var fs_1 = require("fs");
 var util_1 = require("util");
 // promisify the readFile node method to read our txt input files.
 var readInput = util_1.promisify(fs_1.readFile);
+function parseInput(input) {
+    var output = input;
+    var i = 0;
+    var isRunning = true;
+    while (isRunning) {
+    }
+}
 function advent() {
     return readInput("input.txt", "utf8").then(function (input) {
-        console.log(input);
+        var inputStringArray = input.split(",");
+        var inputArray = [];
+        inputStringArray.forEach(function (num) {
+            inputArray.push(parseInt(num));
+        });
     });
 }
 advent();
--- day2/part1/index.ts
+++ day2/part1/index.ts
@@ -4,13 +4,28 @@ import { promisify } from "util";
 // promisify the readFile node method to read our txt input files.
 let readInput = promisify(readFile);
 
+
+function parseInput(input: number[]){
+  let output: number[] = input;
+  let i = 0;
+  let isRunning: boolean = true;
+
+  while(isRunning){
+    
+
+  }
+}
+
 function advent(){
 
   return readInput("input.txt", "utf8").then((input: string) => {
-    let inputStringArray: string[] = input.split("\n");
-    console.log(inputStringArray);
-  })
+    let inputStringArray: string[] = input.split(",");
+    let inputArray: number[] = [];
+    inputStringArray.forEach((num) => {
+      inputArray.push(parseInt(num));
+    })
 
+  })
 }
 
 advent();
