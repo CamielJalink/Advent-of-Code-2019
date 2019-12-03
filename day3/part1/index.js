c80932505fa06d056866f0061d038577f519c850
Camiel Jalink <camiel.jalink@gmail.com>
2019-12-03 21:21:45 +0100

    day3part1 solution

--- day3/part1/index.js
+++ day3/part1/index.js
@@ -13,48 +13,69 @@ function advent() {
     });
 }
 function findClosestCrossing(wire1, wire2) {
-    findWireCoordinates(wire1);
-    findWireCoordinates(wire2);
+    var wire1coords = findWireCoordinates(wire1);
+    var wire2coords = findWireCoordinates(wire2);
+    var allcrossings = findCrossings(wire1coords, wire2coords);
+    var closestCrossingDistance = determineDistance(allcrossings[0].x, allcrossings[0].y);
+    for (var i = 0; i < allcrossings.length; i++) {
+        if (determineDistance(allcrossings[i].x, allcrossings[i].y) < closestCrossingDistance) {
+            closestCrossingDistance = determineDistance(allcrossings[i].x, allcrossings[i].y);
+        }
+    }
+    console.log(closestCrossingDistance);
+    // console.log(closest)
 }
 function findWireCoordinates(wire) {
     var wireCoords = [];
     var currentCoord = { x: 0, y: 0 };
     for (var i = 0; i < wire.length; i++) {
-        var operation = wire[i]; // R105
-        var direction = operation[0]; // R
-        var distance = parseInt(operation.substring(1)); // 105
-        doOperation(direction, distance, currentCoord, wireCoords);
+        var operation = wire[i]; // e.g. R105
+        var direction = operation[0]; //e.g. R
+        var distance = parseInt(operation.substring(1)); //e.g. 105
+        switch (direction) {
+            case 'R':
+                for (var j = 0; j < distance; j++) {
+                    currentCoord.x = currentCoord.x + 1;
+                    wireCoords.push(JSON.parse(JSON.stringify(currentCoord)));
+                }
+                break;
+            case 'D':
+                for (var j = 0; j < distance; j++) {
+                    currentCoord.y = currentCoord.y - 1;
+                    wireCoords.push(JSON.parse(JSON.stringify(currentCoord)));
+                }
+                break;
+            case 'L':
+                for (var j = 0; j < distance; j++) {
+                    currentCoord.x = currentCoord.x - 1;
+                    wireCoords.push(JSON.parse(JSON.stringify(currentCoord)));
+                }
+                break;
+            case 'U':
+                for (var j = 0; j < distance; j++) {
+                    currentCoord.y = currentCoord.y + 1;
+                    wireCoords.push(JSON.parse(JSON.stringify(currentCoord)));
+                }
+                break;
+        }
     }
     return wireCoords;
 }
-function doOperation(direction, distance, currentCoord, wireCoords) {
-    switch (direction) {
-        case 'R':
-            for (var j = 0; j < distance; j++) {
-                currentCoord.x = currentCoord.x + 1;
-                wireCoords.push(currentCoord);
+function findCrossings(wire1coords, wire2coords) {
+    var crossings = [];
+    for (var i = 0; i < wire1coords.length; i++) {
+        for (var j = 0; j < wire2coords.length; j++) {
+            var wire1C = wire1coords[i];
+            var wire2C = wire2coords[j];
+            if (wire1C.x == wire2C.x && wire1C.y == wire2C.y) {
+                crossings.push(JSON.parse(JSON.stringify(wire1C)));
             }
-            break;
-        case 'D':
-            for (var j = 0; j < distance; j++) {
-                currentCoord.y = currentCoord.y - 1;
-                wireCoords.push(currentCoord);
-            }
-            break;
-        case 'L':
-            for (var j = 0; j < distance; j++) {
-                currentCoord.x = currentCoord.x - 1;
-                wireCoords.push(currentCoord);
-            }
-            break;
-        case 'U':
-            for (var j = 0; j < distance; j++) {
-                currentCoord.y = currentCoord.y + 1;
-                wireCoords.push(currentCoord);
-            }
-            break;
+        }
     }
-    return;
+    return crossings;
+}
+function determineDistance(x, y) {
+    return Math.abs(x) + Math.abs(y);
 }
 advent();
 // Twee tests
--- day3/part1/index.ts
+++ day3/part1/index.ts
@@ -17,17 +17,32 @@ function advent(){
 }
 
 
+
 interface Coord {
   x: number,
   y: number
 }
 
 
+
 function findClosestCrossing(wire1: string[], wire2: string[]){
 
-  findWireCoordinates(wire1);
-  findWireCoordinates(wire2);
+  let wire1coords: Coord[] = findWireCoordinates(wire1);
+  let wire2coords: Coord[] = findWireCoordinates(wire2);
+  let allcrossings = findCrossings(wire1coords, wire2coords);
+
+  let closestCrossingDistance: number = determineDistance(allcrossings[0].x, allcrossings[0].y)
+
+  for(let i = 0; i < allcrossings.length; i++){
+    if( determineDistance(allcrossings[i].x, allcrossings[i].y) < closestCrossingDistance){
+      closestCrossingDistance = determineDistance(allcrossings[i].x, allcrossings[i].y);
+    }
+  }
+
+  console.log(closestCrossingDistance);
+  // console.log(closest)
 }
+
 
 
 function findWireCoordinates(wire: string[]){
@@ -35,46 +50,64 @@ function findWireCoordinates(wire: string[]){
   let currentCoord: Coord = {x: 0, y: 0}
 
   for(let i = 0; i < wire.length; i++){
-    let operation: string = wire[i]; // R105
-    let direction: string = operation[0];  // R
-    let distance: number = parseInt(operation.substring(1)); // 105
+    let operation: string = wire[i]; // e.g. R105
+    let direction: string = operation[0];  //e.g. R
+    let distance: number = parseInt(operation.substring(1)); //e.g. 105
 
-    doOperation(direction, distance, currentCoord, wireCoords);
-     }
+    switch (direction) {
+      case 'R':
+        for (let j = 0; j < distance; j++) {
+          currentCoord.x = currentCoord.x + 1;
+          wireCoords.push(JSON.parse(JSON.stringify(currentCoord)));
+        }
+        break;
+      case 'D':
+        for (let j = 0; j < distance; j++) {
+          currentCoord.y = currentCoord.y - 1;
+          wireCoords.push(JSON.parse(JSON.stringify(currentCoord)));
+        }
+        break;
+      case 'L':
+        for (let j = 0; j < distance; j++) {
+          currentCoord.x = currentCoord.x - 1;
+          wireCoords.push(JSON.parse(JSON.stringify(currentCoord)));
+        }
+        break;
+      case 'U':
+        for (let j = 0; j < distance; j++) {
+          currentCoord.y = currentCoord.y + 1;
+          wireCoords.push(JSON.parse(JSON.stringify(currentCoord)));
+        }
+        break;
+    }
+  }
   
   return wireCoords;
 }
 
 
-function doOperation(direction: string, distance: number, currentCoord: Coord, wireCoords: Coord[]){
-  
-  switch(direction) {
-    case 'R':
-      for(let j = 0; j < distance; j++){
-        currentCoord.x = currentCoord.x + 1;
-        wireCoords.push(currentCoord);
+function findCrossings(wire1coords: Coord[], wire2coords: Coord[]){
+
+  let crossings: Coord[] = [];
+
+  for (let i = 0; i < wire1coords.length; i++) {
+    for (let j = 0; j < wire2coords.length; j++) {
+
+      let wire1C: Coord = wire1coords[i];
+      let wire2C: Coord = wire2coords[j];
+
+      if (wire1C.x == wire2C.x && wire1C.y == wire2C.y) {
+        crossings.push(JSON.parse(JSON.stringify(wire1C)));
       }
-      break;
-    case 'D':
-      for(let j = 0; j < distance; j++){
-        currentCoord.y = currentCoord.y - 1;
-        wireCoords.push(currentCoord);
-      }
-      break;
-    case 'L':
-      for(let j = 0; j < distance; j++){
-        currentCoord.x = currentCoord.x - 1; 
-        wireCoords.push(currentCoord);
-      }
-      break;
-    case 'U':
-      for(let j = 0; j < distance; j++){
-        currentCoord.y = currentCoord.y + 1;
-        wireCoords.push(currentCoord);
-      }
-      break;
+    }
   }
-  return 
+
+  return crossings;
+}
+
+
+function determineDistance(x: number, y: number){
+  return Math.abs(x) + Math.abs(y);
 }
 
 
