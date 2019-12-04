87ab149edfbe45c867de92675b3777847b456ae5
Camiel <camiel.jalink@gmail.com>
2019-12-04 09:39:46 +0100

    Day 4 folder created from boilerplate

---
+++ day4/part1/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = require("fs");
+var util_1 = require("util");
+// promisify the readFile node method to read our txt input files.
+var readInput = util_1.promisify(fs_1.readFile);
+function advent() {
+    return readInput("input.txt", "utf8").then(function (input) {
+        var inputStringArray = input.split("\n");
+        var range = inputStringArray[0];
+        console.log(range);
+    });
+}
+advent();
---
+++ day4/part1/index.ts
@@ -0,0 +1,15 @@
+import { readFile } from "fs";
+import { promisify } from "util";
+
+// promisify the readFile node method to read our txt input files.
+let readInput = promisify(readFile);
+
+function advent(){
+
+  return readInput("input.txt", "utf8").then((input: string) => {
+    let passwordRange: string = input; //  "138307-654504"
+  })
+
+}
+
+advent();
