435dc75f127fac770635bdf977ab3f3e987307d9
Camiel <camiel.jalink@gmail.com>
2019-12-04 17:57:20 +0100

    some day4part1 progress

--- day4/part1/index.js
+++ day4/part1/index.js
@@ -6,9 +6,33 @@ var util_1 = require("util");
 var readInput = util_1.promisify(fs_1.readFile);
 function advent() {
     return readInput("input.txt", "utf8").then(function (input) {
-        var inputStringArray = input.split("\n");
-        var range = inputStringArray[0];
-        console.log(range);
+        var passwordRange = input; //  "138307-654504"
+        var stringBounds = passwordRange.split("-");
+        var passwords = validRange(stringBounds);
     });
+}
+function validRange(stringBounds) {
+    var lowerBound = parseInt(stringBounds[0]);
+    var upperBound = parseInt(stringBounds[1]);
+    var allPasswords = [];
+    for (lowerBound; lowerBound <= upperBound; lowerBound++) {
+        allPasswords.push(lowerBound.toString());
+    }
+    return allPasswords;
+}
+function onlyIncrease(passwords) {
+    var validPasswords = [];
+    for (var i = 0; i < passwords.length; i++) {
+        var isValid = true;
+        for (var j = 1; j < passwords[i].length; j++) {
+            if (parseInt(passwords[i][j]) < parseInt(passwords[i][j - 1])) { // If current digit is smaller than previous digit, invalid!
+                isValid = false;
+            }
+        }
+        if (isValid) {
+            validPasswords.push(passwords[i]);
+        }
+        return validPasswords;
+    }
 }
 advent();
--- day4/part1/index.ts
+++ day4/part1/index.ts
@@ -8,8 +8,45 @@ function advent(){
 
   return readInput("input.txt", "utf8").then((input: string) => {
     let passwordRange: string = input; //  "138307-654504"
-  })
+    let stringBounds: string[] = passwordRange.split("-");
 
+    let passwords = validRange(stringBounds);
+    
+  })
+}
+
+
+function validRange(stringBounds: string[]){
+  let lowerBound = parseInt(stringBounds[0]);
+  let upperBound = parseInt(stringBounds[1]);
+  let allPasswords: string[] = [];
+
+  for(lowerBound; lowerBound <= upperBound; lowerBound++){
+    allPasswords.push(lowerBound.toString());
+  }
+  return allPasswords;
+}
+
+
+function onlyIncrease(passwords: string[]){
+  let validPasswords: string[] = [];
+
+  for(let i = 0; i < passwords.length; i++){
+    let isValid: boolean = true; 
+
+    for(let j = 1; j < passwords[i].length; j++){
+
+      if(parseInt(passwords[i][j]) < parseInt(passwords[i][j-1])){  // If current digit is smaller than previous digit, invalid!
+        isValid = false;
+      }
+    }
+
+    if(isValid){
+      validPasswords.push(passwords[i]);
+    }
+
+  return validPasswords;
+  }
 }
 
 advent();
