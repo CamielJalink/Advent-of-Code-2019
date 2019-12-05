0be86c6bfc3c8ab2bf05a073a62c68aa7a515348
Camiel <camiel.jalink@gmail.com>
2019-12-05 09:14:58 +0100

    Part1 project copied to part2

--- day4/part2/index.js
+++ day4/part2/index.js
@@ -6,7 +6,54 @@ var util_1 = require("util");
 var readInput = util_1.promisify(fs_1.readFile);
 function advent() {
     return readInput("input.txt", "utf8").then(function (input) {
-        console.log(input);
+        var passwordRange = input; //  "138307-654504"
+        var stringBounds = passwordRange.split("-");
+        var passwords = validRange(stringBounds);
+        passwords = validIncrease(passwords);
+        passwords = validSamePair(passwords);
+        console.log(passwords.length);
     });
+}
+function validRange(stringBounds) {
+    var lowerBound = parseInt(stringBounds[0]);
+    var upperBound = parseInt(stringBounds[1]);
+    var allPasswords = [];
+    for (lowerBound; lowerBound <= upperBound; lowerBound++) {
+        allPasswords.push(lowerBound.toString());
+    }
+    return allPasswords;
+}
+function validIncrease(passwords) {
+    var validPasswords = [];
+    for (var i = 0; i < passwords.length; i++) {
+        var isValid = true;
+        for (var j = 1; j < passwords[i].length; j++) {
+            // If current digit is smaller than previous digit, password is invalid!
+            if (passwords[i][j] < passwords[i][j - 1]) {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid) {
+            validPasswords.push(passwords[i]);
+        }
+    }
+    return validPasswords;
+}
+function validSamePair(passwords) {
+    var validPasswords = [];
+    for (var i = 0; i < passwords.length; i++) {
+        var isValid = false;
+        // If current digit is the same as previous digit, the password could be legit.
+        for (var j = 1; j < passwords[i].length; j++) {
+            if (passwords[i][j] === passwords[i][j - 1]) {
+                isValid = true;
+            }
+        }
+        if (isValid) {
+            validPasswords.push(passwords[i]);
+        }
+    }
+    return validPasswords;
 }
 advent();
--- day4/part2/index.ts
+++ day4/part2/index.ts
@@ -7,10 +7,71 @@ let readInput = promisify(readFile);
 function advent(){
 
   return readInput("input.txt", "utf8").then((input: string) => {
-    let inputStringArray: string[] = input.split("\n");
-    console.log(inputStringArray);
-  })
+    let passwordRange: string = input; //  "138307-654504"
+    let stringBounds: string[] = passwordRange.split("-");
 
+    let passwords: string[] = validRange(stringBounds);
+    passwords = validIncrease(passwords);
+    passwords = validSamePair(passwords);
+    console.log(passwords.length);
+  })
+}
+
+
+function validRange(stringBounds: string[]){
+  let lowerBound = parseInt(stringBounds[0]);
+  let upperBound = parseInt(stringBounds[1]);
+  let allPasswords: string[] = [];
+
+  for(lowerBound; lowerBound <= upperBound; lowerBound++){
+    allPasswords.push(lowerBound.toString());
+  }
+  return allPasswords;
+}
+
+
+
+function validIncrease(passwords: string[]): string[]{
+  let validPasswords: string[] = [];
+
+  for(let i = 0; i < passwords.length; i++){
+    let isValid: boolean = true; 
+
+    for(let j = 1; j < passwords[i].length; j++){
+      // If current digit is smaller than previous digit, password is invalid!
+      if(passwords[i][j] < passwords[i][j-1]){  
+        isValid = false;
+        break;
+      }
+    }
+
+    if(isValid){
+      validPasswords.push(passwords[i]);
+    }
+  }
+  return validPasswords;
+}
+
+
+
+function validSamePair(passwords: string[]){
+  let validPasswords: string[] = []
+
+  for(let i = 0; i < passwords.length; i++){
+    let isValid: boolean = false; 
+    // If current digit is the same as previous digit, the password could be legit.
+    for(let j = 1; j < passwords[i].length; j++){
+      if(passwords[i][j] === passwords[i][j-1]){
+        isValid = true;
+      }
+    }
+
+    if(isValid){
+      validPasswords.push(passwords[i]);
+    }
+  }
+
+  return validPasswords;
 }
 
 advent();
