46a40e2a5d8fde433774a95393a3ea8dd2d262e7
Camiel Jalink <camiel.jalink@gmail.com>
2019-12-08 19:45:03 +0100

    Day5part2 file copied from part1

--- day5/part2/index.js
+++ day5/part2/index.js
@@ -1,78 +1,101 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = require("fs");
-var util_1 = require("util");
-// promisify the readFile node method to read our txt input files.
-var readInput = util_1.promisify(fs_1.readFile);
-// probably a bit overkill but this function checks whether positions in the inputarray exist.
-function inputIsValid(memory, i) {
-    var positionsValid = true;
-    if (memory[i + 1] > memory.length || memory[i + 2] > memory.length || memory[i + 3] > memory.length) {
-        console.log("Error: illegal memory address");
-        positionsValid = false;
-    }
-    return positionsValid;
+var helpers_1 = require("./helpers");
+// Main function
+function advent() {
+    // runs any tests, then starts current challenge
+    return runTests()
+        .then(function () { return helpers_1.getInput("input.txt")
+        .then(function (inputArray) {
+        runProgram(inputArray, 1);
+    }); });
 }
 // The main logic for this puzzle. Loops over the inputarray and modifies it.
-function intcode(memory) {
-    var i = 0; // i for instruction, of course..
+function runProgram(input, opcodeInput) {
+    var i = 0;
     var isRunning = true;
-    var param1, param2, param3;
+    var opcodeOutputs = [];
     while (isRunning) {
-        param1 = memory[i + 1];
-        param2 = memory[i + 2];
-        param3 = memory[i + 3];
-        switch (memory[i]) {
+        var opcode = helpers_1.parseOpcode(input[i]); // Builds a small array that contains the opcode, and the TYPE of parameters (0 or 1) it has.
+        switch (opcode[0]) { // opcode[0] contains the type of opcode (1 for sum, 2 for multiplication, etc.)
             case 1:
-                if (inputIsValid(memory, i)) {
-                    memory[param3] = memory[param1] + memory[param2];
+                var sum1 = 0, sum2 = 0;
+                if (opcode[1] === 0) { // first param
+                    sum1 = input[input[i + 1]];
                 }
                 else {
-                    isRunning = false;
+                    sum1 = input[i + 1];
                 }
+                if (opcode[2] === 0) { // second param
+                    sum2 = input[input[i + 2]];
+                }
+                else {
+                    sum2 = input[i + 2];
+                }
+                input[input[i + 3]] = sum1 + sum2; // third param is always in position mode
+                i += 4;
                 break;
             case 2:
-                if (inputIsValid(memory, i)) {
-                    memory[param3] = memory[param1] * memory[param2];
+                var mult1 = 0, mult2 = 0;
+                if (opcode[1] === 0) {
+                    mult1 = input[input[i + 1]];
                 }
                 else {
-                    isRunning = false;
+                    mult1 = input[i + 1];
                 }
+                if (opcode[2] === 0) {
+                    mult2 = input[input[i + 2]];
+                }
+                else {
+                    mult2 = input[i + 2];
+                }
+                input[input[i + 3]] = mult1 * mult2;
+                i += 4;
+                break;
+            case 3:
+                // "Parameters that an instruction writes to will never be in immediate mode"   <-- so we don't have to check opcode[1] 
+                if (opcodeInput) {
+                    input[input[i + 1]] = opcodeInput;
+                }
+                else {
+                    throw new Error("No input for opcode 3 was specified");
+                }
+                i += 2;
+                break;
+            case 4:
+                // Add an output to the opcodeOutputs array, based on the parameter mode of opcode[1]
+                if (opcode[1] === 0) {
+                    opcodeOutputs.push(input[input[i + 1]]);
+                }
+                else {
+                    opcodeOutputs.push(input[i + 1]);
+                }
+                i += 2;
                 break;
             case 99:
-                // console.log("Instruction 99, we're done here");
                 isRunning = false;
                 break;
             default:
-                console.log("unexpected instruction found, error!");
+                console.log("unexpected number found, error!");
                 isRunning = false;
         }
-        i += 4;
-        if (i >= memory.length) {
+        if (i >= input.length) {
             isRunning = false;
         }
     }
-    return memory;
+    if (opcodeOutputs.length > 0) {
+        console.log("Opcode 4 output array: " + opcodeOutputs);
+    }
+    return input;
 }
-function advent() {
-    return readInput("input.txt", "utf8").then(function (input) {
-        var inputStringArray = input.split(",");
-        var cleanMemory = [];
-        inputStringArray.forEach(function (num) {
-            cleanMemory.push(parseInt(num));
-        });
-        for (var i = 0; i < 100; i++) {
-            for (var j = 0; j < 100; j++) {
-                var memory = JSON.parse(JSON.stringify(cleanMemory));
-                memory[1] = i;
-                memory[2] = j;
-                if (intcode(memory)[0] == 19690720) {
-                    console.log("noun is " + i);
-                    console.log("verb is " + j);
-                    console.log((i * 100) + j);
-                    j = 999; // Ugly break out of this loop.
-                }
-            }
+function runTests() {
+    return helpers_1.getInput("day2input.txt").then(function (inputArray) {
+        var outputArray = runProgram(inputArray);
+        if (outputArray[0] === 3760627) {
+            console.log("SUCCESS!!! Day2part1 test succesfull");
+        }
+        else {
+            console.log("ERROR!!! Day2part1 test value is " + outputArray[0] + " instead of 3760627");
         }
     });
 }
