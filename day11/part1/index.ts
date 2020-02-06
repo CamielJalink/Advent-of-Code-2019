928d04ad97c91cf105b65c765cc16aac7a3cc566
Camiel <camiel.jalink@gmail.com>
2020-02-06 16:31:39 +0100

    Initial setup for day 11 part 1, still has some bugs in it

--- day11/part1/index.ts
+++ day11/part1/index.ts
@@ -10,20 +10,115 @@ function advent(){
     .then((program: bigint[]) => {
       console.log("starting day 11")
       let computer = new Computer(program);
-      paintShip(computer);
+      console.log(paintShip(computer));
     }))
 }
 
 
 
-function paintShip(computer: Computer){
-
-  let robotLocation: number[] = [0,0];
-  let robotDirection: string = "up";
-
-  console.log(computer.runProgram([0n]));
+interface Square {
+  x: number;
+  y: number;
+  color: number;
 }
 
+
+function paintShip(computer: Computer){
+
+  let numUniqueVisited: number = 0;
+  let knownMap: Square[] = [];
+  let currentLocation: Square = {x: 0, y: 0, color: 0};
+  let currentDirection: string = "up";
+  let nextInput: bigint[] = [];
+
+
+  while(!computer.isFinished){
+
+    nextInput[0] = BigInt(currentLocation.color)
+    let instruction: number[] = computer.runProgram(nextInput).map(bignum => Number(bignum));
+
+
+    currentLocation.color = instruction[0];
+    let isKnownLocation: boolean = false;
+
+    // If the current location has been visited before, update it's color.
+    knownMap.forEach((square: Square) => {
+      if(square.x === currentLocation.x && square.y === currentLocation.y){
+        square.color = currentLocation.color; 
+        isKnownLocation = true;
+      }
+    })
+
+    // If the current location hasn't been visited before, add it to the knownMap and increase numUniqueVisited by 1
+    if(!isKnownLocation){
+      knownMap.push(JSON.parse(JSON.stringify(currentLocation)));
+      numUniqueVisited++;
+    }
+
+
+    // Determine new direction. 
+    switch (currentDirection) {
+      case "up":
+        instruction[1] === 0 ? currentDirection = "left" : currentDirection = "right";
+        break;
+      case "down":
+        instruction[1] === 0 ? currentDirection = "right" : currentDirection = "left";
+        break;
+      case "right":
+        instruction[1] === 0 ? currentDirection = "up" : currentDirection = "down";
+        break;
+      case "left":
+        instruction[1] === 0 ? currentDirection = "down" : currentDirection = "up";
+        break;
+      default:
+        break;
+    }
+    
+    // Determine next position
+    switch (currentDirection) {
+      case "up":
+        currentLocation = checkMap(knownMap, currentLocation.x, currentLocation.y + 1);
+        break;
+      case "down":
+        currentLocation = checkMap(knownMap, currentLocation.x, currentLocation.y - 1);
+        break;
+      case "right":
+        currentLocation = checkMap(knownMap, currentLocation.x + 1, currentLocation.y);
+        break;
+      case "left":
+        currentLocation = checkMap(knownMap, currentLocation.x - 1, currentLocation.y);
+        break;
+      default:
+        break;
+    }
+
+    console.log(knownMap);
+  }
+
+  return numUniqueVisited;
+}
+
+
+
+// Returns the square we are stepping toward if it exists, or creates a new one.
+function checkMap(knownMap: Square[], x: number, y: number){
+  let squareIsKnown: boolean = false;
+  let square: Square;
+
+  knownMap.forEach((existingSquare: Square) => {
+    if(existingSquare.x === x && existingSquare.y === y){
+      square = JSON.parse(JSON.stringify(existingSquare));
+      squareIsKnown = true;
+    }
+  })
+
+  // If we haven't painted this square before, it's color is black and therefor a zero. 
+  if(!squareIsKnown){
+    square = {x: x, y: y, color: 0}
+  }
+
+  return square!;
+}
 
 
 advent();
