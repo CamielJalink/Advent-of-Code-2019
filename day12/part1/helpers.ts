b4ef656a3007c8e1d60b1fe29ffc4d0046584557
Camiel <camiel.jalink@gmail.com>
2020-02-08 12:23:31 +0100

    Day 12 part 1 created from boilerplate

---
+++ day12/part1/helpers.ts
@@ -0,0 +1,14 @@
+import { readFile } from "fs";
+import { promisify } from "util";
+// promisify the readFile node method to read our txt input files.
+
+let readInput = promisify(readFile);
+
+export function getInput(fileName: string){
+  return readInput(fileName, "utf8").then((input: string) => {
+    
+    let inputStringArray: string[] = input.split("\n");
+    return inputStringArray;
+
+  })
+}
---
+++ day12/part1/index.ts
@@ -0,0 +1,26 @@
+import { getInput } from "./helpers";
+
+
+function advent(){
+
+  return runTests().then(() => {
+    return getInput("input.txt").then((input: string[]) => {
+
+
+
+
+      console.log(input);
+    })
+  })
+}
+
+
+
+function runTests(){
+  return getInput("input.txt").then((input: string[]) => {
+    console.log("no tests currently made")
+    return;
+  })
+}
+
+advent();
